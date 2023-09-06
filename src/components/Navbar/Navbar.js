57b54c0a4af4c69e8ac02a844a8b75172f16a5f1
Vaibhavi <porevaibhavi5@gmail.com>
2023-09-06 12:47:21 +0530

    add navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import { React } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+	
+	return (
+    <div className={styles.landingNavbar}>
+      {/* <div className={styles.landingNavbarLogo}> <-- Project Logo
+        <img/>
+      </div> */}
+      <div className={styles.landingNavbarLinks}>
+        <p>
+          <NavLink to="/home">
+            Home
+          </NavLink>
+        </p>
+        <p>
+          <NavLink to="/map">
+            Map
+          </NavLink>
+        </p>
+        <p>          
+          <NavLink to="/">
+            Logout
+          </NavLink>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
