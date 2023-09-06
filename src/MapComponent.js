a3269b5d5798667842d105f63fabbbf7b6532cc7
Vaibhavi <porevaibhavi5@gmail.com>
2023-09-06 12:48:01 +0530

    add navbar to map page

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import customIcon from "./CustomMarkerIcon";
 import "./MapComponent.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function MapComponent({ agencies }) {
   const [userLocation, setUserLocation] = useState(null);
@@ -18,34 +19,38 @@ function MapComponent({ agencies }) {
       }
     );
   }, []);
-  return (
-    <div className="map">
-      <MapContainer
-        center={userLocation || [19.064626928104452, 72.83581727891169]} // Set the initial center of the map
-        zoom={13} // Set the initial zoom level
-        style={{ height: "100vh", width: "100%" }}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
 
-        {agencies.map((agency, index) => (
-          <Marker
-            key={index}
-            position={agency.longlat} // Assuming "location" contains [latitude, longitude] coordinates
-            icon={customIcon}
-          >
-            <Popup>
-              <strong>{agency.name}</strong>
-              <p>Phone: {agency.phoneNumber}</p>
-              <p>Email: {agency.email}</p>
-            </Popup>
-          </Marker>
-        ))}
-      </MapContainer>
-    </div>
+  return (
+    <>
+      <Navbar />
+      <div className="map">
+        <MapContainer
+          center={userLocation || [19.064626928104452, 72.83581727891169]} // Set the initial center of the map
+          zoom={13} // Set the initial zoom level
+          style={{ height: "100vh", width: "100%" }}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+
+          {agencies.map((agency, index) => (
+            <Marker
+              key={index}
+              position={agency.longlat} // Assuming "location" contains [latitude, longitude] coordinates
+              icon={customIcon}
+            >
+              <Popup>
+                <strong>{agency.name}</strong>
+                <p>Phone: {agency.phoneNumber}</p>
+                <p>Email: {agency.email}</p>
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      </div>
+    </>
   );
 }
 
